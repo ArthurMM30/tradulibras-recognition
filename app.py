7f467f95270868878e460e1f4a5863a8e01fc6f1
Helena Barbosa Costa <helenabarcosta01@gmail.com>
2024-10-01 09:32:03 -0300

    right, left, up, down

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import argparse
 import itertools
 from collections import Counter
 from collections import deque
-from unidecode import unidecode
+from unidecode import unidecode  
 
 import cv2 as cv
 import numpy as np
@@ -233,28 +233,28 @@ def main():
                     has_a_new_word = False
 
                 CM = probability_rank[0][0]
-                if 15 < cm_timer  and not has_a_new_word:
-                    result = repo.getSignByCMAndLocal(CM,location)
-                    print("RESULT:" + str(result.get()))
+                # if 15 < cm_timer  and not has_a_new_word:
+                #     result = repo.getSignByCMAndLocal(CM,location)
+                #     print("RESULT:" + str(result.get()))
 
-                    if len(result) == 1:
-                        word = result.getFirstMotto() if language == "pt-br" else result.getFirstMottoEn()
+                #     if len(result) == 1:
+                #         word = result.getFirstMotto() if language == "pt-br" else result.getFirstMottoEn()
 
-                        if len(phrase) == 0 or phrase[-1] != word:
-                            phrase.append(word)
-                            has_a_new_word = True
+                #         if len(phrase) == 0 or phrase[-1] != word:
+                #             phrase.append(word)
+                #             has_a_new_word = True
                     
-                    elif len(result) > 1:
-                        for trajectory_index in most_common_fg_id:
-                            trajectory = point_history_classifier_labels[trajectory_index[0]]
-                            result_filtered = result.filterSignBySense(trajectory)
-                            print("RESULT FILTERED:" + str(result_filtered.get()) + "\nTRAJECTORY:" + trajectory)
-                            if len(result_filtered) == 1:
-                                word = result_filtered.getFirstMotto() if language == "pt-br" else result_filtered.getFirstMottoEn()
-                                if len(phrase) == 0 or phrase[-1] != word:
-                                    phrase.append(word)
-                                    has_a_new_word = True
-                                    break
+                #     elif len(result) > 1:
+                #         for trajectory_index in most_common_fg_id:
+                #             trajectory = point_history_classifier_labels[trajectory_index[0]]
+                #             result_filtered = result.filterSignBySense(trajectory)
+                #             print("RESULT FILTERED:" + str(result_filtered.get()) + "\nTRAJECTORY:" + trajectory)
+                #             if len(result_filtered) == 1:
+                #                 word = result_filtered.getFirstMotto() if language == "pt-br" else result_filtered.getFirstMottoEn()
+                #                 if len(phrase) == 0 or phrase[-1] != word:
+                #                     phrase.append(word)
+                #                     has_a_new_word = True
+                #                     break
             
                 if cm_timer > 150:
                     CM = ""
