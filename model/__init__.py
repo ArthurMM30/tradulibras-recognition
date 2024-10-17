db04806e72774c63f2ec42c486d258b5f10109c0
Rafaella Guimarães Venturini <rafaella.venturini@germinare.org.br>
2024-10-17 09:52:30 -0300

    feat: rotation

--- model/__init__.py
+++ model/__init__.py
@@ -1,2 +1,7 @@
+from model.point_history_classifier.point_history_classifier import (
+    PointHistoryClassifier,
+)
+from model.rotation_history_classifier.rotation_history_classifier import (
+    RotationHistoryClassifier,
+)
 from model.keypoint_classifier.keypoint_classifier import KeyPointClassifier
-from model.point_history_classifier.point_history_classifier import PointHistoryClassifier
--- repository/signsDescription.py
+++ repository/signsDescription.py
@@ -1,12 +1,15 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
-class SignsDescriptionClient():
+class SignsDescriptionClient:
     def __init__(self):
-        self.client = MongoClient("mongodb://localhost:27017/")
-        self.dbs = self.client['TraduLibras']
-        self.collection = self.dbs['signsDescription']
-
+        self.client = MongoClient(os.getenv("MONGO_URL"))
+        self.dbs = self.client["TraduLibras"]
+        self.collection = self.dbs["signsDescription"]
 
     def getSignByCM(self, data_request, index=0, is_dominant=True):
         data_response = []
@@ -18,21 +21,33 @@ class SignsDescriptionClient():
 
         return SignsDescriptionEntity(data_response)
 
-    def getSignByCMAndLocal(self, data_request_cm, data_request_local, index=0, is_dominant=True):
+    def getSignByCMAndLocal(
+        self,
+        data_request_cm,
+        data_request_local,
+        data_request_rotation,
+        index=0,
+        is_dominant=True,
+    ):
         data_response = []
         hand = "dominant_hand" if is_dominant else "auxiliar_hand"
         cm_query = {f"phonology.{str(index)}.{hand}.CM": data_request_cm}
         local_query = {f"phonology.{str(index)}.{hand}.final_local": data_request_local}
+        rotation_query = {
+            f"phonology.{str(index)}.{hand}.rotation": data_request_rotation
+        }
 
-        for sign in self.collection.find({"$and":[cm_query, local_query]}):
+        for sign in self.collection.find(
+            {"$and": [cm_query, local_query, rotation_query]}
+        ):
             data_response.append(sign)
 
         return SignsDescriptionEntity(data_response)
-    
+
     def close(self):
         self.client.close()
 
-    
+
 class SignsDescriptionEntity(object):
     def __init__(self, data):
         self.data = data
@@ -60,13 +75,13 @@ class SignsDescriptionEntity(object):
         return SignsDescriptionEntity(data_response)
 
     def get(self):
-        return self.data    
+        return self.data
 
     def getFirstMotto(self):
-        return self.data[0]["motto"] 
+        return self.data[0]["motto"]
 
     def getFirstMottoEn(self):
         return self.data[0]["motto_en"]
-    
+
     def __len__(self):
         return len(self.data)
